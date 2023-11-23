95a4f8bf085ad9826b7f5d7238d7cfb2bd12b85c
WebGCDev <gaetano.chi@gmail.com>
2023-11-23 16:08:17 +0100

    feat: add bundle 1 esercizio 2 completo

--- bundle-01/main.js
+++ bundle-01/main.js
@@ -26,8 +26,23 @@ function addIfEven(num) {
     return num;
 }
 
+/* Il codice nella condizione if ha solamente un operatore di uguaglianza assegnandone il suo relativo valore, invece noi come da esempio scritto dobbiamo ottenere un confronto e quindi dobbiamo utilizzare una funzione comparativa NUM %2 === 0 (perchè chiediamo di dividere il numero per 2 se il resto è 0 allora facciamo il return di num+5, ma il nostro scopo è vedere se il resto è uguale a 0)
+La soluzione finale è questa, per essere corretta la funzione bisogna salvarla in una variabile al di fuori di essa :
 
-// ESERCIZIO 3
+
+function addIfEven(num) {
+    if (num % 2 === 0) {
+        return num + 5;
+    }
+    return num;
+}
+
+*/
+
+
+
+
+ESERCIZIO 3
 function loopToFive() {
     for (let i = 0, i < 5, i++) {
         console.log(i);
